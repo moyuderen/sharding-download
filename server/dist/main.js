3bc5a9cd9ebbf031ca7f9b84e6a32ee26279dbab
moyuderen <moyuderen2021@163.com>
2025-05-06 15:24:21 +0800

    feat: swagger文档

--- server/dist/main.js
+++ server/dist/main.js
@@ -20,7 +20,9 @@ async function bootstrap() {
         .setVersion('1.0')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, options);
-    swagger_1.SwaggerModule.setup('api', app, document);
+    swagger_1.SwaggerModule.setup('api', app, document, {
+      customCssUrl: '/static/swagger-ui.css',
+    });
     app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'), {
         prefix: '/static',
         setHeaders: (res, path) => {
